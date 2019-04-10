1b043fdb7cb152783cd3471f7fb022dc4677859f
AlexRebega <rebega.alex@gmail.com>
2019-04-10 19:14:37 +0300

    initial commit

---
+++ app/src/main/java/com/ip/alexrebega/mbooking/SearchActivity.java
@@ -0,0 +1,26 @@
+package com.ip.alexrebega.mbooking;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Settings page openning...", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
