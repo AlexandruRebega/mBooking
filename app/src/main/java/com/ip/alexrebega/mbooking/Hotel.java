ffe5582a557b81bd0bba4c62891cd2ddc1c47691
AlexRebega <rebega.alex@gmail.com>
2019-04-15 00:46:48 +0300

    hotel attributes

--- app/src/main/java/com/ip/alexrebega/mbooking/Hotel.java
+++ app/src/main/java/com/ip/alexrebega/mbooking/Hotel.java
@@ -1,6 +1,67 @@
 package com.ip.alexrebega.mbooking;
 
+
 public class Hotel {
+
+    private int imageId;
+    private String mHotelName;
+    private int mRooms;
+    private double mPrice;
+    private float mRating;
+
+    public Hotel() {
+//        imageId = context.getResources.getDrawable...
+        mHotelName = "Great Hotel";
+        mRooms = 4;
+        mPrice = 132.24;
+        mRating = (float)2.5;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+
+    public void setImageId(int imageId) {
+        this.imageId = imageId;
+    }
+
+    public String getmHotelName() {
+        return mHotelName;
+    }
+
+    public void setmHotelName(String mHotelName) {
+        this.mHotelName = mHotelName;
+    }
+
+    public int getmRooms() {
+        return mRooms;
+    }
+
+    public void setmRooms(int mRooms) {
+        if(mRooms < 0)
+            this.mRooms = 0;
+        else
+            this.mRooms = mRooms;
+    }
+
+    public double getmPrice() {
+        return mPrice;
+    }
+
+    public void setmPrice(double mPrice) {
+        if(mPrice < 0)
+            this.mPrice = 0;
+        else
+            this.mPrice = mPrice;
+    }
+
+    public float getmRating() {
+        return mRating;
+    }
+
+    public void setmRating(float mRating) {
+        this.mRating = mRating;
+    }
 
 }
 
