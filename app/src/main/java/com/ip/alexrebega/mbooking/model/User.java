12efe007e0583350d97326bb8fbe519593b722e2
AlexRebega <rebega.alex@gmail.com>
2019-05-12 14:09:17 +0300

    user entity

---
+++ app/src/main/java/com/ip/alexrebega/mbooking/model/User.java
@@ -0,0 +1,25 @@
+package com.ip.alexrebega.mbooking.model;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+import java.util.List;
+
+@Entity
+public class User {
+    @PrimaryKey
+    public int uid;
+
+    @ColumnInfo(name = "mail")
+    public String mail;
+
+    @ColumnInfo(name = "reservations")
+    public List<Integer> reservationsId;
+
+    @ColumnInfo(name = "first_name")
+    public String firstName;
+
+    @ColumnInfo(name = "last_name")
+    public String lastName;
+}
